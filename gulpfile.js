e3d96d7f7176e7a6a2de891f310398e14e604d98
Arthur Rump <arthurrump@outlook.com>
2016-07-02 17:34:58 +0200

    Add bootstrap (stylus port)
    colors.style is now replaced with bootstrap's variables.styl
    gulp adds some bootstrap js together to assets/js/bootstrap.min.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var stylint = require('gulp-stylint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 var stylFiles = './assets/styl/*.styl';
+
+function bsjs(file) {
+    return './assets/bootstrap-stylus-5.0.2/js/' + file + '.js'
+};
+var bootstrapJsFiles = [bsjs('collapse'), bsjs('button'), bsjs('dropdown')];
 
 gulp.task('stylus-lint', function() {
     return gulp.src(stylFiles)
@@ -23,8 +30,15 @@ gulp.task('stylus-compile', function() {
 
 gulp.task('stylus', ['stylus-lint', 'stylus-compile']);
 
+gulp.task('bootstrap-js', function() {
+    return gulp.src(bootstrapJsFiles)
+        .pipe(concat('bootstrap.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./assets/js'))
+});
+
 gulp.task('watch', function() {
     gulp.watch(stylFiles, ['stylus']);
 });
 
-gulp.task('default', ['stylus']);
+gulp.task('default', ['stylus', 'bootstrap-js']);
